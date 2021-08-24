0b600625c1f17676f69d564012cd761645e8919e
timsch3 <timsch3@gmail.com>
2021-08-24 16:33:53 +0200

    shortened js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,4 @@
-function reverseWordsInString(string) {
-    if (string.includes(' ')) {
-        let words = string.split(' ')
-        return words.map(elt => reverseWord(elt)).join(' ')
-    }
-    else {
-        return reverseWord(string)
-    }
-}
-function reverseWord(word) {
-    let letters = word.split('').reverse()
-    return letters.join('')
-}
-
 function reverseInputString() {
-    document.getElementById('output').innerHTML = reverseWordsInString(document.getElementById('textInput').value)
+    let string = document.getElementById('textInput').value
+    document.getElementById('output').innerHTML = string.includes(' ') ? string.split(' ').map(elt => elt.split('').reverse().join('')).join(' ') : string.split('').reverse().join('')
 }
